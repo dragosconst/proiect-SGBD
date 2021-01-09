50477e6b84c5e0e41c574545818a2ea36458e611
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-01-09 17:28:40 +0200

    final form + no pdf

--- ex13.sql
+++ ex13.sql
@@ -1,71 +1,71 @@
 CREATE OR REPLACE PACKAGE proiect AS
     TYPE int_table IS VARRAY(3) OF NUMBER;
     
-    PROCEDURE ex6 (v_nume_carte carte.denumire%TYPE);
+    PROCEDURE ex6 (v_nume_carte carti.denumire%TYPE);
     PROCEDURE ex7 (vechime NUMBER,
-                   cod_librarie librarie.librarie_id%TYPE);
+                  cod_librarie librarii.librarie_id%TYPE);
     FUNCTION f8 (job_name jobs.denumire%TYPE)
                 RETURN int_table;
-    PROCEDURE p9 (lib_name librarie.denumire%TYPE);
+    PROCEDURE p9 (lib_name librarii.denumire%TYPE);
 END proiect;
 /
 
 CREATE OR REPLACE PACKAGE BODY proiect AS
-    PROCEDURE ex6 (v_nume_carte carte.denumire%TYPE) IS
-    TYPE string_tabel IS TABLE OF VARCHAR2(60) INDEX BY PLS_INTEGER;
-    TYPE number_tabel IS TABLE OF NUMBER INDEX BY PLS_INTEGER;
-    v_id_carte carte.carte_id%TYPE := NULL;
-    v_librarii string_tabel;
-    v_abonamente string_tabel;
-    v_pret abonament.plata_lunara%TYPE;
+    PROCEDURE ex6 (v_nume_carte carti.denumire%TYPE )IS
+        TYPE string_tabel IS TABLE OF VARCHAR2(60) INDEX BY PLS_INTEGER;
+        TYPE number_tabel IS TABLE OF NUMBER INDEX BY PLS_INTEGER;
+        v_id_carte carti.carte_id%TYPE := NULL;
+        v_librarii string_tabel;
+        v_abonamente string_tabel;
+        v_pret abonamente.plata_lunara%TYPE;
     BEGIN
-    SELECT carte_id INTO v_id_carte
-    FROM carte
-    WHERE v_nume_carte = carte.denumire;
-    
-    SELECT l.denumire BULK COLLECT INTO v_librarii
-    FROM librarie l, se_afla_in sai
-    WHERE sai.carte_id = v_id_carte AND sai.librarie_id = l.librarie_id;
-    
-    DBMS_OUTPUT.put(v_nume_carte || ' se gaseste in librariile: ');
-    FOR i IN 1..v_librarii.last LOOP
-        DBMS_OUTPUT.put(v_librarii(i) || ' ');
-    END LOOP;
-    DBMS_OUTPUT.NEW_LINE();
+        SELECT carte_id INTO v_id_carte
+        FROM carti
+        WHERE v_nume_carte = carti.denumire;
         
-    SELECT a.abonament_id BULK COLLECT INTO v_abonamente
-    FROM abonament a, carte_inclusa ci
-    WHERE ci.carte_id = v_id_carte AND ci.abonament_id = a.abonament_id;
-    
-    IF v_abonamente.count = 0 THEN
-        DBMS_OUTPUT.PUT_LINE('Cartea nu este inclusa in niciun abonament.');
-    ELSE
-            DBMS_OUTPUT.PUT(v_nume_carte || ' este inclusa in format digital in abonamentele: ');
-        FOR i in 1..v_abonamente.last LOOP
-            SELECT a.plata_lunara INTO v_pret
-            FROM abonament a
-            WHERE a.abonament_id = v_abonamente(i);
-            DBMS_OUTPUT.PUT('abonamentul ' || v_abonamente(i) || ' cu pretul ' || v_pret || ', ');
+        SELECT l.denumire BULK COLLECT INTO v_librarii
+        FROM librarii l, se_afla_in sai
+        WHERE sai.carte_id = v_id_carte AND sai.librarie_id = l.librarie_id;
+        
+        DBMS_OUTPUT.put(v_nume_carte || ' se gaseste in librariile: ');
+        FOR i IN 1..v_librarii.last LOOP
+            DBMS_OUTPUT.put(v_librarii(i) || ' ');
         END LOOP;
         DBMS_OUTPUT.NEW_LINE();
-    END IF;
-    
-    EXCEPTION 
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Cartea nu exista.');
+            
+        SELECT a.abonament_id BULK COLLECT INTO v_abonamente
+        FROM abonamente a, carte_inclusa ci
+        WHERE ci.carte_id = v_id_carte AND ci.abonament_id = a.abonament_id;
+        
+        IF v_abonamente.count = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('Cartea nu este inclusa in niciun abonament.');
+        ELSE
+                DBMS_OUTPUT.PUT(v_nume_carte || ' este inclusa in format digital in abonamentele: ');
+            FOR i in 1..v_abonamente.last LOOP
+                SELECT a.plata_lunara INTO v_pret
+                FROM abonamente a
+                WHERE a.abonament_id = v_abonamente(i);
+                DBMS_OUTPUT.PUT('abonamentul ' || v_abonamente(i) || ' cu pretul ' || v_pret || ', ');
+            END LOOP;
+            DBMS_OUTPUT.NEW_LINE();
+        END IF;
+        
+        EXCEPTION 
+            WHEN NO_DATA_FOUND THEN
+                DBMS_OUTPUT.PUT_LINE('Cartea nu exista.');
     END ex6;
-    
+
     PROCEDURE ex7 (vechime NUMBER,
-                  cod_librarie librarie.librarie_id%TYPE)
+                  cod_librarie librarii.librarie_id%TYPE)
     IS
-        CURSOR c (pvec NUMBER, pcod librarie.librarie_id%TYPE) IS
+        CURSOR c (pvec NUMBER, pcod librarii.librarie_id%TYPE) IS
             SELECT a.angajat_id, a.nume, a.prenume, a.manager_id
-            FROM angajat a, lucreaza_in li
+            FROM angajati a, lucreaza_in li
             WHERE TRUNC ((SYSDATE) - a.data_angajarii) / 365.5 >= pvec AND li.angajat_id = a.angajat_id AND li.librarie_id = pcod;
     BEGIN
         FOR i in c(vechime, cod_librarie) LOOP
             IF i.manager_id IS NOT NULL THEN
-                UPDATE angajat
+                UPDATE angajati
                 SET salariu = salariu + salariu * 15 / 100
                 WHERE angajat_id = i.angajat_id;
                 DBMS_OUTPUT.PUT_LINE('A fost marit salariul anagajtului\ei ' || i.nume || ' ' || i.prenume || ' .');
@@ -74,9 +74,9 @@ CREATE OR REPLACE PACKAGE BODY proiect AS
     END ex7;
     
     FUNCTION f8 (job_name jobs.denumire%TYPE)
-                    RETURN int_table
+                        RETURN int_table
     IS
-        v_job_id angajat.job_id%TYPE;
+        v_job_id angajati.job_id%TYPE;
         v_check NUMBER;
         v_old_lib_val NUMBER := -1;
         v_counter NUMBER := 1;
@@ -88,14 +88,14 @@ CREATE OR REPLACE PACKAGE BODY proiect AS
         WHERE denumire = job_name;
         
         SELECT COUNT(*) INTO v_check -- verific daca exista angajati cu job-ul dat
-        FROM angajat a
+        FROM angajati a
         WHERE a.job_id = v_job_id;
         IF v_check = 0 THEN
             RAISE_APPLICATION_ERROR(-20000, 'Nu exista angajati cu job-ul dat.');
         END IF;
             
         FOR i IN (SELECT l.librarie_id, a.job_id, COUNT(*) c
-                FROM angajat a, lucreaza_in li, librarie l
+                FROM angajati a, lucreaza_in li, librarii l
                 WHERE a.angajat_id = li.angajat_id AND li.librarie_id = l.librarie_id
                 GROUP BY l.librarie_id, a.job_id
                 ORDER BY l.librarie_id) LOOP
@@ -121,25 +121,24 @@ CREATE OR REPLACE PACKAGE BODY proiect AS
         EXCEPTION
             WHEN NO_DATA_FOUND THEN
                 RAISE_APPLICATION_ERROR(-20000, 'Nu exista jobul dat');
-    END f8;
+    END f8;   
     
-    PROCEDURE p9 (lib_name librarie.denumire%TYPE)
+    PROCEDURE p9 (lib_name librarii.denumire%TYPE)
     IS
-    TYPE nume IS RECORD (abonament_id abonament.abonament_id%TYPE,
-                        nume membru.nume%TYPE,
-                        prenume membru.prenume%TYPE);
+    TYPE nume IS RECORD (abonament_id abonamente.abonament_id%TYPE,
+    nume membri.nume%TYPE,
+    prenume membri.prenume%TYPE);
     TYPE name_table IS TABLE OF nume INDEX BY PLS_INTEGER;
-    v_lib_id librarie.librarie_id%TYPE;
+    v_lib_id librarii.librarie_id%TYPE;
     flag NUMBER := 0;
     v_data name_table;
     BEGIN
         SELECT librarie_id INTO v_lib_id
-        FROM librarie
+        FROM librarii
         WHERE denumire = lib_name;
-        flag := 1;
         
         SELECT a.abonament_id, m.nume, m.prenume BULK COLLECT INTO v_data
-        FROM membru m, abonament a, carte_inclusa ci, carte c, se_afla_in sai
+        FROM membri m, abonamente a, carte_inclusa ci, carti c, se_afla_in sai
         WHERE sai.librarie_id = v_lib_id AND sai.carte_id = c.carte_id AND c.carte_id = ci.carte_id AND ci.abonament_id = a.abonament_id AND m.abonament_id = a.abonament_id
         GROUP BY a.abonament_id, m.nume, m.prenume;
         
@@ -153,9 +152,7 @@ CREATE OR REPLACE PACKAGE BODY proiect AS
         
         EXCEPTION
             WHEN NO_DATA_FOUND THEN
-                IF flag = 0 THEN
-                    RAISE_APPLICATION_ERROR(-20000, 'Nu exista libraria ceruta.');
-                END IF;
-    END p9;
+                RAISE_APPLICATION_ERROR(-20000, 'Nu exista libraria ceruta.');
+    END p9;      
 END proiect;
 /
